4580c6e8b2d85dcdbb9e0987b072763a1c40f736
jiaangou <wou@zoology.ubc.ca>
2024-01-30 16:46:51 -0800

    added an example for downloading files

--- scripts/osf_link.R
+++ scripts/osf_link.R
@@ -8,22 +8,41 @@ library(dplyr)
 #Retrieve project info
 project <- osf_retrieve_node("mjvwh")
 
-#List files uploaded into the project 
-osf_ls_files(project)
+#Authentication
+PAT <- Sys.getenv("PAT_KEY") #gets key from .Renviron
+osf_auth(PAT)
 
-#List components of the project 
-osf_ls_nodes(project)
+#List of components in the project 
+components <- osf_ls_nodes(project)
+data_component <- osf_retrieve_node('qha69') #get path for the data component
 
-#Create a "Data" component
-project%>%
-  osf_create_component("Data")
 
-#Make subirectories in the Data component
+#Create a "Data" component (there are already 2 Data components)
+# project%>%
+#   osf_create_component("Data")
+
+
+#Make subdirectories in the Data component
 new_directories <- c('rawdata', 'tabular_shapefiles', 'outdata')
-sapply(new_directories, function(x)osf_mkdir(name = x, target = "Data"))
+sapply(new_directories, function(x)osf_mkdir(data_component, x))
+
+#Check that directories are added
+osf_retrieve_node('qha69')%>%
+  osf_ls_files()
 
 
 ############################
 # Accessing files ----------
 ############################
+
+#Download csv file from DATA component as an example
+osf_retrieve_node('qha69')%>% #list files in data component
+  osf_ls_files()%>%
+  filter(name == "CIEE_Canada_data_upload.csv")%>% #filter out the file that is being downloaded
+  osf_download(path = "./osf_data")
+
+
+
+
+
 
