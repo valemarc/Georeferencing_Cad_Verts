b9fa6f69398945d2cadee84dbb1a655e0dcc44c6
jiaangou <wou@zoology.ubc.ca>
2024-02-01 14:57:31 -0800

    reformatted script

--- scripts/osf_link.R
+++ scripts/osf_link.R
@@ -16,11 +16,9 @@ osf_auth(PAT)
 components <- osf_ls_nodes(project)
 data_component <- osf_retrieve_node('qha69') #get path for the data component
 
-
 #Create a "Data" component (there are already 2 Data components)
 # project%>%
 #   osf_create_component("Data")
-
 
 #Make subdirectories in the Data component
 new_directories <- c('rawdata', 'tabular_shapefiles', 'outdata')
@@ -29,7 +27,6 @@ sapply(new_directories, function(x)osf_mkdir(data_component, x))
 #Check that directories are added
 osf_retrieve_node('qha69')%>%
   osf_ls_files()
-
 
 ############################
 # Accessing files ----------
@@ -40,8 +37,6 @@ osf_retrieve_node('qha69')%>% #list files in data component
   osf_ls_files()%>%
   filter(name == "CIEE_Canada_data_upload.csv")%>% #filter out the file that is being downloaded
   osf_download(path = "./osf_data")
-
-
 
 
 
