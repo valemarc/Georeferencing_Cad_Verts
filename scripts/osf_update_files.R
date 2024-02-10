c2a5c3afcac365c26b6fb35e8c964a77626a43fc
jiaangou <wou@zoology.ubc.ca>
2024-02-09 16:39:39 -0800

    general functions for uploading and downloading from osf

--- scripts/osf_update_files.R
+++ scripts/osf_update_files.R
@@ -1,29 +1,56 @@
 ############################
-#Update files to OSF
+#Updating OSF files
 ############################
 library(osfr)
 
+#Functions -------
+
+# 1. Downloading
+download_osf_files <- function(project,
+                               component){
+  
+  project%>%
+    osf_ls_nodes()%>%
+    filter(name == component)%>%
+    osf_ls_files()%>%
+    osf_download(conflicts = "overwrite")
+  
+  
+}
+
+# 2. Uploading
+upload_osf_files <- function(project, component, files){
+  
+  project%>%
+    osf_ls_nodes()%>%
+    filter(name == component)%>%
+    osf_upload(path = files, conflicts = "overwrite")
+  
+}
+
+
+
 #Create an example file locally so we can push it to OSF
-write.csv(iris, file = "iris.csv")
-write.csv(cars, file = 'cars.csv')
-
-#Upload file to data component 
-#osf_upload(data_component, path = 'iris.csv') #individually
-osf_upload(data_component, path = list.files(pattern = ".csv$")) #in batch
-
-#List files in the component to check that its been uploaded
-data_component%>%
-  osf_ls_files() 
-
-#Update files in batches 
-data_component%>%
-  osf_upload(path = list.files(pattern = '.csv$'), conflicts = "overwrite")
-
-
-#Delete example files
-files <- c('iris.csv', 'cars.csv')
-sapply(files, function(x)data_component%>%
-         osf_ls_files()%>%
-         filter(name == x)%>%
-         osf_rm())
-
+# write.csv(iris, file = "iris.csv")
+# write.csv(cars, file = 'cars.csv')
+# 
+# #Upload file to data component 
+# #osf_upload(data_component, path = 'iris.csv') #individually
+# osf_upload(data_component, path = list.files(pattern = ".csv$")) #in batch
+# 
+# #List files in the component to check that its been uploaded
+# data_component%>%
+#   osf_ls_files() 
+# 
+# #Update files in batches 
+# data_component%>%
+#   osf_upload(path = list.files(pattern = '.csv$'), conflicts = "overwrite")
+# 
+# 
+# #Delete example files
+# files <- c('iris.csv', 'cars.csv')
+# sapply(files, function(x)data_component%>%
+#          osf_ls_files()%>%
+#          filter(name == x)%>%
+#          osf_rm())
+# 
