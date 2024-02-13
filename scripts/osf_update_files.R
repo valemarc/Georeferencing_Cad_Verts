581435601d06b21b3117edaafaa3f0e24e5eb368
jiaangou <wou@zoology.ubc.ca>
2024-02-12 16:11:32 -0800

    removed download and upload calls to prevent changes being overwritten

--- scripts/osf_update_files.R
+++ scripts/osf_update_files.R
@@ -2,9 +2,10 @@
 #Updating OSF files
 ############################
 library(osfr)
+library(dplyr)
+
 
 #Functions -------
-
 # 1. Downloading
 download_osf_files <- function(project,
                                component){
@@ -29,28 +30,16 @@ upload_osf_files <- function(project, component, files){
 }
 
 
+#Authentication
+PAT <- Sys.getenv("PAT_KEY") #gets key from .Renviron
+osf_auth(PAT)
 
-#Create an example file locally so we can push it to OSF
-# write.csv(iris, file = "iris.csv")
-# write.csv(cars, file = 'cars.csv')
-# 
-# #Upload file to data component 
-# #osf_upload(data_component, path = 'iris.csv') #individually
-# osf_upload(data_component, path = list.files(pattern = ".csv$")) #in batch
-# 
-# #List files in the component to check that its been uploaded
-# data_component%>%
-#   osf_ls_files() 
-# 
-# #Update files in batches 
-# data_component%>%
-#   osf_upload(path = list.files(pattern = '.csv$'), conflicts = "overwrite")
-# 
-# 
-# #Delete example files
-# files <- c('iris.csv', 'cars.csv')
-# sapply(files, function(x)data_component%>%
-#          osf_ls_files()%>%
-#          filter(name == x)%>%
-#          osf_rm())
-# 
+#Retrieve node 
+project <- osf_retrieve_node("mjvwh")
+
+#Download 
+#project%>%
+#  download_osf_files(project = ., component = "Data")
+
+#Upload
+#upload_osf_files(project, component = "Data", files = c('outdata','rawdata','tabular_shapefiles'))
